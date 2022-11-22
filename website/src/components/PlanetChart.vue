64782538f86fb834ae5ca71f1dcdbfe79e1480d6
Nicholas Thompson <github@hoekwil.com>
2022-11-22 19:43:39 +0200

    Sort of working

--- website/src/components/PlanetChart.vue
+++ website/src/components/PlanetChart.vue
@@ -15,9 +15,46 @@ export default {
       planetChartData: planetChartData,
     };
   },
-  mounted() {
-    const ctx = document.getElementById("planet-chart");
-    new Chart(ctx, this.planetChartData);
+  async mounted() {
+    this.loaded = false;
+
+    try {
+      const ctx = document.getElementById("planet-chart");
+      const resp = await fetch(
+        "https://us-central1-bietsbot.cloudfunctions.net/httpServer/day"
+      );
+      const dayData = await resp.json();
+      const timeSeries = dayData.map((a) => a.time);
+      const dataSeries = dayData.map((a) => a.temperature);
+      this.planetChartData.data.labels = timeSeries;
+      this.planetChartData.data.datasets[0].data = dataSeries;
+      new Chart(ctx, this.planetChartData);
+      this.loaded = true;
+    } catch (e) {
+      console.error(e);
+    }
   },
 };
 </script>
+
+<!-- 
+export default {
+    name: 'BarChart',
+    components: { Bar },
+    data: () => ({
+      loaded: false,
+      chartData: null
+    }),
+    async mounted () {
+      this.loaded = false
+  
+      try {
+        const { userlist } = await fetch('/api/userlist')
+        this.chartdata = userlist
+  
+        this.loaded = true
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  } -->
--- website/src/planet-data.js
+++ website/src/planet-data.js
@@ -1,14 +1,11 @@
 export const planetChartData = {
   type: "line",
   data: {
-    labels: [
-      1669013331, 1669013632, 1669013931, 1669014231, 1669014531, 1669014831,
-      1669015131, 1669015431,
-    ],
+    labels: [],
     datasets: [
       {
-        label: "Number of Moons",
-        data: [0, 0, 1, 2, 79, 82, 27, 134],
+        label: "Temperature",
+        data: [],
         backgroundColor: "rgba(54,73,93,.5)",
         borderColor: "#36495d",
         borderWidth: 3,
